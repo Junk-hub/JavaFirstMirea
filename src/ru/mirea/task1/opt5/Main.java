3ffae74fb7017a02f4cad48fee4f31a6286760a5
Junk-hub <1orangecactus@gmail.com>
2021-09-21 11:51:02 +0300

    -

--- src/ru/mirea/task1/opt5/Main.java
+++ src/ru/mirea/task1/opt5/Main.java
@@ -1,8 +1,7 @@
 package ru.mirea.task1.opt5;
 
 public class Main {
-
-    static void task3(){
+    public static void main(String[] args) {
         System.out.println();
         System.out.println("Первые 10 элементов гармонического ряда: ");
 
@@ -12,10 +11,5 @@ public class Main {
             n = 1/i;
             System.out.print(String.format("%,.2f", (1./i)) + "  ");
         }
-
-    }
-
-    public static void main(String[] args) {
-        task3();
     }
 }
